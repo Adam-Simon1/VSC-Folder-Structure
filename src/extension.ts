f2e3bd66ceb473eb6ed02988d8d9899e5f7a7bea
Adam-Simon1 <adamsimonmin@gmail.com>
2023-11-10 17:05:20 +0100

    Creates the structure and puts it to clipboard

--- src/extension.ts
+++ src/extension.ts
@@ -3,29 +3,39 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "folder-structure.copyStructure",
-    (uri: vscode.Uri) => {
+    async (uri: vscode.Uri) => {
       // Get the folder uri the user right clicked on and executed the command
       // Read the directory contents based on the uri and crawl the whole folder
       // Process the results to a structure
       // Copy the structure to the clipboard
 
-      listFiles(uri);
+      const structure = await listFiles(uri);
+      const structureString = JSON.stringify(structure, null, 2);
+      vscode.env.clipboard.writeText(structureString);
     }
   );
 
   context.subscriptions.push(disposable);
 }
 
-async function listFiles(uri: vscode.Uri) {
+async function listFiles(uri: vscode.Uri): Promise<Record<string, any>> {
   const result = await vscode.workspace.fs.readDirectory(uri);
+  let structure: Record<string, any> = {};
 
-  result.forEach(([name, type]) => {
-	if (type === 2) {
-		listFiles(vscode.Uri.joinPath(uri, name));
-	}
-  });
+  await Promise.all(
+    result.map(async ([name, type]) => {
+      if (type === vscode.FileType.Directory) {
+        const path = vscode.Uri.joinPath(uri, name);
 
-  console.log(result);
+        // Recursively process subdirectories
+        structure[name] = await listFiles(path);
+      } else {
+        structure[name] = "File";
+      }
+    })
+  );
+
+  return structure;
 }
 
 export function deactivate() {}
