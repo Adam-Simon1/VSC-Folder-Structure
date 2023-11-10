f404f0b190c39c74ac960bc27308b73bd7431c2a
Adam-Simon1 <adamsimonmin@gmail.com>
2023-11-10 16:29:17 +0100

    Lists contents from the rightclicked folder

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "folder-structure.copyStructure",
+    (uri: vscode.Uri) => {
+      // Get the folder uri the user right clicked on and executed the command
+      // Read the directory contents based on the uri and crawl the whole folder
+      // Process the results to a structure
+      // Copy the structure to the clipboard
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fs-structure-copy" is now active!');
+      listFiles(uri);
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fs-structure-copy.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('VS-Code');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+async function listFiles(uri: vscode.Uri) {
+  const result = await vscode.workspace.fs.readDirectory(uri);
+
+  console.log(result);
+}
+
 export function deactivate() {}
